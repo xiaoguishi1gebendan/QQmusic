25f817bd83179f6ecbe8750195d6ae01cc6c8b28
huihui <1208078915@qq.com>
2018-03-14 19:02:35 +0800

    歌手列表数据已经渲染完毕

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,17 @@
 <template>
  <div>
-singer
+<mt-index-list>
+  <mt-index-section v-for="(item,index) in arrlist" :key="index" :index="item.title">
+    <mt-cell v-for="(ite,inde) in item.arr" :key="inde" >
+      <img  v-lazy="ite.imgarc" alt="">
+      <p>{{ite.name}}</p>
+    </mt-cell>
+   
+  </mt-index-section>
+</mt-index-list>
+
+
+
  </div>
 </template>
 
@@ -11,8 +22,7 @@ import {getSingerList} from "../../api/singer.js"
  export default {
    data () {
      return {
-      
-       Singerindex:[]
+       arrlist:[ ]
      }
    },
    mounted () {
@@ -23,49 +33,67 @@ import {getSingerList} from "../../api/singer.js"
        getSingerList().then((res)=>{
          var  SingerList=[];
          SingerList=res.data.list;
-          // 定义热歌部分
-          const HOTTITLE="热歌"
-          var hot={
-                    title:HOTTITLE,
-                    item:[]
-                  };
-          // 补充热歌部分，前十的数据歌手为热歌歌手
-          const HOTLEN=10;
-          SingerList.forEach((ite,index)=>{
-            if(index<HOTLEN){
-              hot.item.push({
-              id:ite.Fsinger_mid,
-              name:ite.Fsinger_name,
-              imgsrc:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${ite.Fsinger_mid}.jpg?max_age=2592000`
-            })
-            }
-             // A-Z的歌手组合
-          var key=ite.Findex;
-          if(!this.Singerindex[key]){
-            this.Singerindex[key]={
-              title:key,
-              item:[]
+         console.log(SingerList)
+         
+        //  数据重组
+      SingerList.forEach((ele,index) => {
+        this.arrlist.push(ele.Findex);
+      });
+       for(var i = 0; i <this.arrlist.length; i++){
+          for( var j = i + 1; j < this.arrlist.length; j++){
+            if(this.arrlist[i] == this.arrlist[j]){
+              this.arrlist.splice(j,1);
+              j--;
             }
           }
-          this.Singerindex[key].item.push({
-            id:ite.Fsinger_mid,
-              name:ite.Fsinger_name,
-              imgsrc:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${ite.Fsinger_mid}.jpg?max_age=2592000`
-          })
-          })
-          this.Singerindex.push(hot);
-        
-          this.Singerindex.sort((a,b)=>{
-            if (a.toString().toLowerCase() > b.toString().toLowerCase()) {
-            return 1;
-              }else{
-                return -1;
-              }
-          })
-            console.log( this.Singerindex);
-         
+        }
+        var reg=/[a-z,A-Z]/g;
+        this.arrlist=this.arrlist.join("").match(reg)
+        // console.log(reg.exec(this.arrlist.join("")))
+          this.arrlist.sort();
 
+      // 把所有字符的都组成对象放到数组里
+      var Singerindex=[];
+        SingerList.forEach((item,index)=>{
+          Singerindex.push({
+               title:item.Findex,
+               arr:[]
+             })
+        })
+      // 相同的字符的对象去重
+      for(var i = 0; i < Singerindex.length; i++){
+          for( var j = i + 1; j < Singerindex.length; j++){
+            if(Singerindex[i].title == Singerindex[j].title){
+              Singerindex.splice(j,1);
+              j--;
+            }
+          }
+        }
 
+      // 排序
+      var arr=[];
+      for(var i=0;i<this.arrlist.length;i++){
+        for(var j=0;j<Singerindex.length;j++){
+          if(this.arrlist[i]==Singerindex[j].title){
+            this.arrlist[i]=Singerindex[j]
+          }
+        }
+      }
+  
+
+      this.arrlist.forEach((ite,inde)=>{
+        SingerList.forEach((itee,indee)=>{
+            if(ite.title==itee.Findex){
+              ite.arr.push({
+                name:itee.Fsinger_name,
+                id:itee.Fsinger_mid,
+                imgarc:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${itee.Fsinger_mid}.jpg?max_age=2592000`
+              })
+            }
+        })
+      })
+
+    console.log(this.arrlist)
 
 
 
@@ -79,6 +107,60 @@ import {getSingerList} from "../../api/singer.js"
 </script>
 
 <style>
-
+#aa{
+  color:#fff; 
+}
+.mint-cell-title{
+  color: gainsboro;
  
+}
+ .mint-indexlist-nav{
+    color: gainsboro ;
+ }
+ .mint-indexsection-index{
+   color: gainsboro;
+ }
+ .mint-cell-wrapper div{
+   float: left;
+ }
+ .mint-cell-title{
+   flex: none;
+ }
+ .mint-indexsection-index{
+   background-color:#333;
+    border: none;
+    padding: 0.2rem 0.4rem;
+ }
+ .mint-cell-wrapper{
+background-image:none;
+padding: 0.2rem 0.4rem;
+ }
+ .mint-indexlist-nav{
+   border-left: none;
+   background: #000;
+   height: 8.8rem;
+ }
+ .mint-cell{
+   background: transparent;
+   background: #222;
+ }
+ .mint-indexlist-nav{
+   display: block;
+   border-radius: 0.4rem;
+   width: 0.5rem;
+   padding-top: 0.4rem;
+   margin-top: 1.4rem !important;
+ }
+ .mint-cell:last-child{
+   background-image:none;
+ }
+ .mint-cell img{
+   width: 60px;
+    vertical-align: middle;
+    border-radius: 50%;
+    
+ }
+ .mint-cell-value p{
+   padding-left: 0.3rem;
+ }
 </style>
